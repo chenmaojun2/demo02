a00051dad70da663e84913dfcc90878508e26197
CMJ-2020 <111111>
2020-12-18 20:23:38 +0800

    添加了 for循环

--- src/main/java/cn/tedu/HelloWorld.java
+++ src/main/java/cn/tedu/HelloWorld.java
@@ -5,5 +5,12 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello World!!!");
+
+        int sum = 0;
+
+        for (int i = 0; i < 10; i++) {
+            sum += i;
+        }
+        System.out.println("总数为: " + sum);
     }
 }
